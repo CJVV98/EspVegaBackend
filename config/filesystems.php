3aef9e571ad5f3bc5a5d4f1b52b2eae47b539c5f
Corin Viracacha <jazminv181@hotmail.com>
2021-02-01 10:18:52 -0500

    Cambios

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,7 +66,7 @@ return [
     */
 
     'links' => [
-        public_path('images') => storage_path('app/images')
+        public_path('images') => storage_path('app/images'),
         public_path('documents') => storage_path('app/documents')
     ],
 
